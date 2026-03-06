c0d6d1f092a91be237310ea10d381c64bd2e302a
unknown <udaykaple5@gmail.com>
2026-03-07 03:11:29 +0530

    cache removed

--- server/index.js
+++ server/index.js
@@ -12,7 +12,11 @@ const PORT = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-app.use("/uploads", express.static("uploads"))
+app.use("/uploads", express.static("uploads", {
+  setHeaders: (res) => {
+    res.setHeader("Cache-Control", "no-store")
+  }
+}))
 
 /* -------------------------
    MULTER CONFIG
